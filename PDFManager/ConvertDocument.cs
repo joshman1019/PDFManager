60e9d15d4740116e6d675019eb9100d81ea46d42
Joshua Santiago <joshman1019@gmail.com>
2021-09-13 17:08:09 -0400

    Created framework for document conversion class

---
+++ PDFManager/ConvertDocument.cs
@@ -0,0 +1,20 @@
+using PDFManager.Core;
+using System; 
+
+namespace PDFManager
+{
+    public static class ConvertDocument
+    {
+        public static byte[] ConvertDocumentToByteArray(string path)
+        {
+            // TODO: Complete this section
+            throw new NotImplementedException(); 
+        }
+
+        public static string ConvertByteArrayToDocument(IPDFDocument document)
+        {
+            // TODO: Complete this section
+            throw new NotImplementedException(); 
+        }
+    }
+}
